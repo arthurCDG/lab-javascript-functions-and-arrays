8559c4c4febcdc69e8addf29cf631099dccfe54b
Arthur Chastenet <arthur.chastenet@gmail.com>
2021-12-02 18:29:33 +0100

    Completed up to Iteration of Uniquified Array

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,43 +1,142 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(number1, number2) {
+  return (number1 > number2) ? number1 : number2;
+}
 
 // Iteration #2: Find longest word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(array) {
+  
+  if (array.length === 0) {
+    return null;
+  } else if (array.length === 1) {
+    return array[0];
+  } else {
 
+    let longestWord = array[0];
+  
+    for (let i = 1; i < array.length; i++) {
+      if (longestWord.length === array[i].length) {
+        continue;
+      } else if (longestWord.length < array[i].length) {
+        longestWord = array[i]
+      }
+    }
+    return longestWord;
 
+  }
+
+}
 
 // Iteration #3: Calculate the sum
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(array) {
+  
+  let sum = 0;
 
+  if (array.length === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+  }
 
+  return sum;
+
+}
 
 // Iteration #3.1 Bonus:
-function sum() {}
 
+numbers = [3, 4, 6, 10];
 
+function sum(array) {
+
+  let sum = 0;
+
+  if (array.length === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      
+      if (typeof array[i] === 'object') {
+        throw new Error("Unsupported data type sir or ma'am");
+      }else if (typeof array[i] === 'string') {
+        sum += array[i].length;
+      } else {
+        sum += Number(array[i]);
+      }
+      
+    }
+  }
+
+  return sum;
+
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
+function averageNumbers(array) {
+  
+  if (array.length === 0) {
+    return null;
+  } else {
+    const avg = array.reduce((a,b) => a + b) / array.length;
+    return avg;
+  }
+  
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(array) {
+
+  let sum = 0;
+
+  if (array.length === 0) {
+    return null;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i].length;
+    }
+  }
+
+  return sum / array.length;
+
+}
 
 // Bonus - Iteration #4.1
-function avg() {}
+
+let arrayTest = [6, 12, 'miami', 1, 'barca', '200', 'lisboa', 8, false];
+
+function avg(array) {
+
+  let sum = 0;
+
+  if (array.length === 0) {
+    return null;
+  } else {
+    
+    for (let i = 0; i < array.length; i++) {
+      if (typeof array[i] === 'string') {
+        sum += array[i].length;
+      } else {
+        sum += Number(array[i]);
+      }
+    }
+    
+  }
+
+  return Math.round((sum / array.length)*100)/100;
+
+}
+
+console.log(avg(arrayTest));
 
 // Iteration #5: Unique arrays
+
 const wordsUnique = [
   'crab',
   'poison',
@@ -52,9 +151,53 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+/* ----- Tentative compliquée qui a échoué -----
 
+function uniquifyArray(array) {
 
+  let uniquifiedArray = []
+
+  if (array.length === 0) {
+    return null;
+  } else {
+
+    uniquifiedArray.push(array[0]);
+
+    for (let i = 1; i < array.length; i++) {
+      for (let j = 0; j < uniquifiedArray.length; j++) {
+        if (array[i] !== uniquifiedArray[j]) {
+          uniquifiedArray.push(array[i]);
+        }
+      }
+      
+    }
+  }
+
+   return uniquifiedArray;
+
+}
+
+*/ 
+
+function uniquifyArray(array) {
+
+  let uniquifiedArray = [];
+
+  if (array.length === 0) {
+    return null;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (!(uniquifiedArray.includes(array[i]))) {
+        uniquifiedArray.push(array[i]);
+      } 
+    }
+  }
+
+  return uniquifiedArray;
+
+}
+
+console.log(uniquifyArray(wordsUnique));
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
